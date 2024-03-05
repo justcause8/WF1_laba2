f23a07bb6874a3bda8420835a916db2e67e28405
justsauce <dmzverev8@gmail.com>
2024-03-05 20:37:36 +0800

    Добавьте файлы проекта.

---
+++ WinFormsApp1_laba2/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WinFormsApp1_laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            width = new TextBox();
+            height = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // width
+            // 
+            width.Location = new Point(263, 51);
+            width.Name = "width";
+            width.Size = new Size(125, 27);
+            width.TabIndex = 0;
+            width.TextChanged += textBox1_TextChanged;
+            width.KeyUp += Ctrl_KeyUp;
+            // 
+            // height
+            // 
+            height.Location = new Point(263, 100);
+            height.Name = "height";
+            height.Size = new Size(125, 27);
+            height.TabIndex = 1;
+            height.KeyUp += Ctrl_KeyUp;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(245, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Введите ширину прямоугольника";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(238, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Введите высоту прямоугольника";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 143);
+            button1.Name = "button1";
+            button1.Size = new Size(376, 39);
+            button1.TabIndex = 2;
+            button1.Text = "Посчитать квадраты";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.KeyUp += Ctrl_KeyUp;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 15);
+            button2.Name = "button2";
+            button2.Size = new Size(376, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Задание";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 194);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(height);
+            Controls.Add(width);
+            Name = "Form1";
+            Text = "Задание 2 (вар.16)";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox width;
+        private TextBox height;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ WinFormsApp1_laba2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WinFormsApp1_laba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // считывем значения из настроек
+            width.Text = Properties.Settings.Default.widht.ToString();
+            height.Text = Properties.Settings.Default.height.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputWidht, inputHeight;
+            try
+            {
+                inputWidht = this.width.Text;
+                inputHeight = this.height.Text;
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+
+            //  передаем введенные значения в параметры
+            Properties.Settings.Default.widht = inputWidht;
+            Properties.Settings.Default.height = inputHeight;
+            Properties.Settings.Default.Save(); // сохраняем переданные значения, чтобы они восстановились пре очередном запуске
+
+            var outMassage = Logic.Compare(inputWidht, inputHeight);
+            MessageBox.Show(outMassage);
+        }
+
+        public class Logic
+        {
+            public static string Compare(string inputWidht, string inputHeight)
+            {
+                string outMessage = "";
+                int count = 0;
+
+                if (CheckInput(inputHeight) && CheckInput(inputWidht))
+                {
+                    int width = int.Parse(inputWidht);
+                    int height = int.Parse(inputHeight);
+
+                    outMessage = "Квадраты имеют размерности: ";
+                    for (; width >= 1 && height >= 1; count++)
+                    {
+                        if (width < height)
+                        {
+                            int temp = width;
+                            width = height;
+                            height = temp;
+                        }
+                        width -= height;
+
+                        outMessage += height + " ";
+                    }
+                    outMessage += $"\nКоличество квадратов: {count}\n";
+                }
+                else
+                {
+                    outMessage = "Ошибка ввода\n";
+                }
+                return outMessage;
+            }
+            static bool CheckInput(string size)
+            {
+                return Regex.IsMatch(size, @"^[0-9]+$");
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ctrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Дан прямоугольник с размерами a × b. От него отрезают квадраты максимального размера, пока это возможно. " +
+                "Затем от оставшегося прямоугольника вновь отрезают квадраты максимально возможного размера и т. д. " +
+                "На какие квадраты и в каком их количестве будет разрезан исходный прямоугольник?");
+        }
+    }
+}
